c75f240e2b95ef522ae46cd9b00a7ce1133b8850
Jay9621 <87541442+Jay9621@users.noreply.github.com>
2024-08-10 14:11:07 +0000

    promise

--- 09_Advance_one/promises.js
+++ 09_Advance_one/promises.js
@@ -84,3 +84,16 @@ async function consumePromiseFive() {
 }
 
 consumePromiseFive()
+
+async function getAllUsers() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json() // json me convert hone ko time lagta hai to ise bhi await karavna padega
+        console.log(data);
+        
+    } catch (error) {
+        console.log("E: ", error);
+        
+    }
+}
+getAllUsers()
